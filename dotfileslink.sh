d58978d0bd8f9a6a5b5695f4a4b1b59f2d60e110
icFukkun <ic151234@edu.okinawa-ct.ac.jp>
2018-05-18 09:31:40 +0900

    anything

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -3,4 +3,4 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.latexmk ~/.latexmk
+ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
